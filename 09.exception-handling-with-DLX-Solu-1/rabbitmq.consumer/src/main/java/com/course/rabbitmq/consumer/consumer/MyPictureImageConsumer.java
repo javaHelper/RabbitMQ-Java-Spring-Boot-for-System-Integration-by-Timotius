8233a95f90e51ddd0aaecf764862c73125e05db4
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-12-29 19:38:02 +0530

    Exception Handling with DLX Solu-1

---
+++ 09.exception-handling-with-DLX-Solu-1/rabbitmq.consumer/src/main/java/com/course/rabbitmq/consumer/consumer/MyPictureImageConsumer.java
@@ -0,0 +1,41 @@
+package com.course.rabbitmq.consumer.consumer;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Service;
+
+import com.course.rabbitmq.consumer.entity.Picture;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
+
+@Service
+public class MyPictureImageConsumer {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MyPictureImageConsumer.class);
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@RabbitListener(queues = "q.mypicture.image")
+	public void listen(String message, 
+			Channel channel, 
+			@Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
+
+		var picture = objectMapper.readValue(message, Picture.class);
+
+		if (picture.getSize() > 9000) {
+			throw new AmqpRejectAndDontRequeueException("Picture size too large : " + picture);
+		}
+
+		LOG.info("On image : {}", picture);
+		channel.basicAck(tag, false);
+	}
+
+}
